231b1fffd97065e30b0561a6afd4ca948d89aae3
santiagogutierrez <santiagoguba@unisabana.edu.co>
2024-10-25 00:07:22 -0500

    vercel

--- fe-proyectofinal/src/App.jsx
+++ fe-proyectofinal/src/App.jsx
@@ -1,13 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Register from './pages/Register';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
