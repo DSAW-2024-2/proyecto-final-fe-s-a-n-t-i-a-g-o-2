38f53dd5b6b408d29092fe4a62056478cee6fcbb
santiagogutierrez <santiagoguba@unisabana.edu.co>
2024-10-25 00:12:28 -0500

    app.jsx

--- fe-proyectofinal/src/App.jsx
+++ fe-proyectofinal/src/App.jsx
@@ -1,14 +1,11 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AuthPage from './pages/authPage';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<AuthPage />} />
       </Routes>
     </Router>
   );
