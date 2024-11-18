b789eecbd46436c1d3359021d22402305fb73c19
santiagogutierrez <santiagoguba@unisabana.edu.co>
2024-11-17 21:21:35 -0500

    Update API base URL and modify request interceptor to include authorization token

--- fe-proyectofinal/src/services/api.js
+++ fe-proyectofinal/src/services/api.js
@@ -2,18 +2,15 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Ajusta esta URL según tu configuración
+  baseURL: 'https://proyecto-final-be-s-a-n-t-i-a-g-o-2.vercel.app', // Cambia esto si tu backend está en otra URL
 });
 
-// Interceptor para añadir el token de autorización a cada solicitud
+// Interceptor para añadir el token a las peticiones
 api.interceptors.request.use(
   (config) => {
     const storedUser = JSON.parse(localStorage.getItem('user'));
     if (storedUser && storedUser.token) {
       config.headers.Authorization = `Bearer ${storedUser.token}`;
-    } else {
-      // Si no hay token, eliminar el header de autorización
-      delete config.headers.Authorization;
     }
     return config;
   },
